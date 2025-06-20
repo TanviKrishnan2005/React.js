7d72dd74bd02cec314f95727bd19e6121e7a0430
Tanvi <rmtanvikrishnan@gmail.com>
2025-06-20 19:22:28 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Resturant from "./component/Basics/Resturant";
 import UseState from './component/Hooks/useState';
-
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
 const App = () => {
   // return <Resturant />;
-  return <UseState/>;
+  //return <UseState/>;
+  //return <UseEffect/>;
+  return <UseReducer/>;
 
 };
 
