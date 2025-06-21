542dc50e0c1abc76c0d9737d430acd5e8b0ae151
Tanvi <rmtanvikrishnan@gmail.com>
2025-06-21 13:15:42 +0530

    todo updated

--- src/component/TODO React/todo.js
+++ src/component/TODO React/todo.js
@@ -1,33 +1,79 @@
-import React ,{useState}from 'react';
+import React, { useState, useEffect } from 'react';
 import "./style.css";
 
+//get localstorage data back 
+const getLocalData = () => {
+  const lists = localStorage.getItem("mytodolist");
+  if (lists) {
+    return JSON.parse(lists);
+  }
+  else {
+    return [];
+  }
+};
+
 const Todo = () => {
-    const [inputdata, setInputData]=React.useState("");
-    const [items, setItems] = useState([]);
+  const [inputdata, setInputData] = React.useState("");
+  const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
 
-    //add the items function
-    const addItem=()=>{
-        if(!inputdata){
-            alert('please fill the data')
-        }else{
-            const myNewInputData ={
-                id: new Date().getTime().toString(),
-                name:inputdata,
-            }
-            setItems([...items,myNewInputData ]);
-            setInputData("");
-        }
-    };
+  //add the items function
+  const addItem = () => {
+    if (!inputdata) {
+      alert('please fill the data')
+    } else if (inputdata && toggleButton) {
+      setItems(
+        items.map((curElem) => {
+          if (curElem.id === isEditItem) {
+            return { ...curElem, name: inputdata }
+          }
+          return curElem;
+        })
+      );
 
-    //how to delete items section
-    const deleteItem=(index)=>{
-    const updatedItems = items.filter((curElem)=>{
-        return curElem.id !== index;
+      setInputData([]);
+      setIsEditItem(null);
+      setToggleButton(false);
+    } else {
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: inputdata,
+      }
+      setItems([...items, myNewInputData]);
+      setInputData("");
+    }
+  };
+
+  //edit the items
+  const editItem = (index) => {
+    const item_todo_edited = items.find((curElem) => {
+      return curElem.id === index;
+    });
+    setInputData(item_todo_edited.name)
+    setIsEditItem(index);
+    setToggleButton(true);
+  };
+
+  //how to delete items section
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((curElem) => {
+      return curElem.id !== index;
     });
     setItems(updatedItems)
-    };
-  return (
+  };
 
+  // remove all the elements
+  const removeAll = () => {
+    setItems([]);
+  }
+
+  //adding local storage
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(items));
+  }, [items]);
+
+  return (
     <>
       <div className="main-div">
         <div className="child-div">
@@ -42,31 +88,37 @@ const Todo = () => {
               placeholder="🖋️ Add item"
               className="form-control"
               value={inputdata}
-              onChange={(event)=>setInputData(event.target.value)}
+              onChange={(event) => setInputData(event.target.value)}
             />
-            <i class="fa-solid fa-plus add-btn" onClick={addItem}></i>
+            {toggleButton ? (
+              <i className="far fa-edit add-btn" onClick={addItem}></i>
+            ) : (
+              <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            )}
+
           </div>
           {/* show items */}
           <div className="showItem">
-
-                {items.map((curElem)=>{
-                    return(
-                        <div className="eachItem" key ={curElem.id} >
-                <h3>{curElem.name}</h3>
-                <div className="todo-btn">
-                    <i class="fa-solid fa-pen-to-square add-btn"></i>
-                    <i class="fa-solid fa-trash add-btn"onClick={()=>{deleteItem(curElem.id)}}></i>
+            {items.map((curElem) => {
+              return (
+                <div className="eachItem" key={curElem.id} >
+                  <h3>{curElem.name}</h3>
+                  <div className="todo-btn">
+                    <i class="fa-solid fa-pen-to-square add-btn" onClick={() => { editItem(curElem.id) }}></i>
+                    <i class="fa-solid fa-trash add-btn" onClick={() => { deleteItem(curElem.id) }}></i>
+                  </div>
                 </div>
-                </div>
-                    );
-                })}
+              );
+            })}
           </div>
-            {/* remove all */}
+
+          {/* remove all */}
           <div className="showItems">
-             <button
-              className="btn effect04" data-sm-link-text="Remove All">
-                <span>CHECK LIST</span>
-            
+            <button
+              className="btn effect04"
+              data-sm-link-text="Remove All"
+              onClick={removeAll}>
+              <span> CHECK LIST</span>
             </button>
           </div>
         </div>
